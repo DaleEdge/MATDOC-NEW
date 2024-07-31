cd936ecefd1daa13938e25f27ee2648c6223b552
Dale Edge <daleedgeofficial@gmail.com>
2024-07-31 16:25:02 +0530

    footer.blade.php

--- resources/views/frontend/include/footer.blade.php
+++ resources/views/frontend/include/footer.blade.php
@@ -6,9 +6,9 @@
                 <div class="col-lg-4 col-sm-6">
                     <!-- Footer Widget Start -->
                     <div class="footer-widget">
-                        <h4 class="footer-widget-title">About Campus Bull</h4>
+                        <h4 class="footer-widget-title">About MatDoc</h4>
                         <div class="widget-info">
-                            <p> We at Campus Bull(A unit of Globe Consulting Services), helps students in getting the besest posible college in their desied Branch.</p>
+                            <p> We at MatDoc, help students in getting the best possible college in their desired Branch.</p>
                         </div>
                     </div>
                     <!-- Footer Widget End -->
@@ -45,10 +45,10 @@
                     <div class="footer-widget">
                         <h4 class="footer-widget-title">Contact Us</h4>
                         <div class="widget-info">
-                            <p><i class="flaticon-phone-call"></i> <a href="tel:+91-9341888827">+91-9341888827</a>, <a href="tel:+91-9036344111">+91-9036344111</a></p>
+                            <p><i class="flaticon-phone-call"></i> <a href="tel:+91-6369149396">+91-6369149396</a>, <a href="tel:+91-8015958614">+91-8015958614</a></p>
                             <p><i class="flaticon-placeholder"></i> <a
-                                    href="mailto:info@campus_bull.com">info@campus_bull.com</a></p>
-                            <p><i class="flaticon-open-mail"></i> <span>17, JDR complex new bel road Bengaluru (560094)</span>
+                                    href="mailto:info@matdoc.in">info@matdoc.in</a></p>
+                            <p><i class="flaticon-open-mail"></i> <span>639, Veeravanji St Vandiyur Madurai TamilNadu (625020)</span>
                             </p>
                             <ul class="social">
                                 <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
@@ -63,7 +63,7 @@
         <!-- Footer Widget Wrapper End -->
         <!-- Footer Copyright End -->
         <div class="footer-copyright text-center">
-            <p>&copy;2024 Campus Bull (A unit of Globe Consulting Services) All rights reserved. </p>
+            <p>&copy;2024 MatDoc All rights reserved. </p>
         </div>
         <!-- Footer Copyright End -->
     </div>
@@ -73,7 +73,7 @@
 
 <div class="whatsp">
     <!--- whatsapp icon start here -->
-    <a href="https://wa.me/9341888827" target="_blank">
+    <a href="https://wa.me/6369149396" target="_blank">
         <img src="{{ asset('front_css/assets/images/neet-whatsapp.png') }}">
     </a>
 </div>
@@ -82,7 +82,7 @@
 <script>
     jQuery(function($) {
         var path = window.location.href;
-        if (path == 'https://campusbull.com/') {} else {
+        if (path == 'https://matdoc.in/') {} else {
             // because the 'href' property of the DOM element is the absolute path
             $('.main-menu li a').removeClass('active');
             $('.main-menu li a').each(function() {
