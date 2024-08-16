a94254ca40bbaf822ab098efd4bbafad2aca00f2
ThiyagarajRKO <ThiyagarajRKO@gmail.com>
2024-08-16 15:26:10 +0530

    fixed row issue

--- resources/views/backend/admin/allotment/index.blade.php
+++ resources/views/backend/admin/allotment/index.blade.php
@@ -30,26 +30,28 @@
                     </thead>
                     <tbody>
                         @forelse ($list as $key => $data)
+                            <tr>
+                                <td>{{$key + 1}}</td>
+                                <td>{{$data->round}}</td>
+                                <td>{{$data->state_rank}}</td>
+                                <td>{{$data->state}}</td>
+                                <td>{{$data->institute}}</td>
+                                <td>{{$data->course}}</td>
+                                <td>{{$data->quota}}</td>
+                                <td>
+                                    <a class="btn btn-success  btn-sm mr-1" href="{{route('allotment.edit', $data->id)}}"><i
+                                            class="far fa-edit"></i></a>
 
-                            <td>{{$key + 1}}</td>
-                            <td>{{$data->round}}</td>
-                            <td>{{$data->state_rank}}</td>
-                            <td>{{$data->state}}</td>
-                            <td>{{$data->institute}}</td>
-                            <td>{{$data->course}}</td>
-                            <td>{{$data->quota}}</td>
-                            <td>
-                                <a class="btn btn-success  btn-sm mr-1" href="{{route('allotment.edit', $data->id)}}"><i
-                                        class="far fa-edit"></i></a>
-
-                                {!! Form::open(['method' => 'DELETE', 'route' => ['allotment.destroy', $data->id], 'style' => 'display:inline']) !!}
-                                <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
-                                {!! Form::close() !!}
-                            </td>
+                                    {!! Form::open(['method' => 'DELETE', 'route' => ['allotment.destroy', $data->id], 'style' => 'display:inline']) !!}
+                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                                    {!! Form::close() !!}
+                                </td>
+                            </tr>
                         @empty
                             <tr>
                                 <td colspan="10" class="text-center text-info">Opps!! There Are No Data Found..</td>
                             </tr>
+
                         @endforelse
                     </tbody>
                 </table>
